64a70c66b5043d7e0921f370cf156edb11e1d578
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-08-30 18:36:52 -0400

    update

--- app/src/js/db/read_and_show_data.js
+++ app/src/js/db/read_and_show_data.js
@@ -30,7 +30,25 @@ function read_and_show_data()
 
 /*
 Item: Kevlar Vest | Price: $450.5
-Item: Field Medical Kit | Price: $75.25
+Item: Night Vision Goggles | Price: $2500
+Item: Combat Boots | Price: $89.99
+Item: Tactical Radio | Price: $320
+Item: MRE Case | Price: $45
+Item: Ammunition Crate | Price: $180
+Item: Portable Generator | Price: $1200
+Item: Camouflage Netting | Price: $60
+Item: Titanium Grappling Hook | Price: $145
+Item: Thermal Imaging Drone | Price: $3500
+Item: Ballistic Helmet | Price: $320.75
+Item: Heavy Breach Door Ram | Price: $450
+Item: Waterproof GPS Navigator | Price: $210.5
+Item: Armored Plate Carrier | Price: $195
+Item: Laser Rangefinder | Price: $280.99
+Item: Paracord Rescue Rope | Price: $25
+Item: Ghillie Suit | Price: $120
+Item: Drop-Leg Holster | Price: $45.5
+Item: Carbon Fiber Crossbow | Price: $850
+Item: Tactical Tomahawk | Price: $95
 */
 
 //----//
